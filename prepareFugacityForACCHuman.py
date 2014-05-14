308d9a4db7a759ee24da42e21c49dc55ee5fd15e
Stephen A. Wood <stephen.wood@mail.utoronto.ca>
2014-05-14 12:13:33 -0400

    Making stand-alone version

--- prepareFugacityForACCHuman.py
+++ prepareFugacityForACCHuman.py
@@ -8,15 +8,20 @@ import BETRS
 def run():
 	write_fugacity_file()
 
-def write_fugacity_file():
-
-	chemical_name = "PCB-153" # Modify as needed
-	filename_path = '../Output/default/pcb153 run/dyn_out.cpk' # Modify as needed
-	time_step = '0730' # In Hours
+def write_fugacity_file(chemical_name = 'PCB-153',
+						filename_path = '../Output/default/pcb153 run/dyn_out.cpk',
+						time_step = '0730',
+						grid_cells = [77, 75, 77, 77]
+						): # Modify these parameters as needed. Will write interactive file to control this one.
 
 	# Using BETR-Research v1.0
 
 	results = BETRS.load(filename_path) # Load the results
+
+	if len(grid_cells) is not 4:
+		raise Exception("Error, invalid number of grid_cells")
+
+	air_region, seawater_region, soil_region, freshwater_region = grid_cells
 
 	# Retrieve fugacities from relevant environmental media
 	# Format:
@@ -25,11 +30,11 @@ def write_fugacity_file():
 	# Units - 'Pa' is fugacity - required for ACC-Human.
 	# grid_cell: The grid cell from the BETR-Research globe. Remember to subtract 1 from the actual zone due to indexing starting at 0.
 
-	air = results[2]['Pa'][77,:] # Lower air compartment, Fugacity (Pa), Region / grid cell from 78
-	seawater = results[5]['Pa'][75,:]
-	soil = results[6]['Pa'][77,:]
-	freshwater = results[4]['Pa'][77,:]
-	groundwater = freshwater # Assuming same contamination in groundwater as freshwater
+	air_fugacity = results[2]['Pa'][air_region - 1,:] # Lower air compartment, Fugacity (Pa), Region / grid cell from 78
+	seawater_fugacity = results[5]['Pa'][seawater_region - 1,:]
+	soil_fugacity = results[6]['Pa'][soil_region - 1,:]
+	freshwater_fugacity = results[4]['Pa'][freshwater_region - 1,:]
+	groundwater_fugacity = freshwater_fugacity # Assuming same contamination in groundwater as freshwater
 
 	# 	Open File for Output
 	#	Because ACC-Human is windows based, I must use the \r\n for End Of Line (EOL)
@@ -41,7 +46,7 @@ def write_fugacity_file():
 		fugacity_file.write('2100') 	# End year for ACC-Human
 
 		# Loop through fugacities and write to file
-		for f1, f2, f3, f4, f5 in zip(air, seawater, soil, freshwater, groundwater):
+		for f1, f2, f3, f4, f5 in zip(air_fugacity, seawater_fugacity, soil_fugacity, freshwater_fugacity, groundwater_fugacity):
 			fugacity_file.write('\r\n')
 			fugacity_file.write(' {:.3E}'.format(float(f1)))
 			fugacity_file.write(' {:.3E}'.format(float(f2)))
