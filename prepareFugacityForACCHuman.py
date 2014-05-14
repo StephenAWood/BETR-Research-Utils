79f7705976077b77d2153273d7bdc6c4868ca9ad
Stephen A. Wood <stephen.wood@mail.utoronto.ca>
2014-05-14 09:44:38 -0400

    Added some comments

--- prepareFugacityForACCHuman.py
+++ prepareFugacityForACCHuman.py
@@ -6,54 +6,57 @@ import BETRS
 
 def write_fugacity_file():
 
-	#data = fugacityReader.openFile("BETR-Global Output/PCB-153 BETR-Global Fugacities Temp Fix.csv")
-
-	#acquireFugacityForRegionAndCompartment(region, compartment, data)
-
-	chemicalName = "PCB-153"
+	chemical_name = "PCB-153"
+	filename_path = '../Output/default/pcb153 run/dyn_out.cpk'
 
 	# Using BETR-Research v1.0
 
-	results = BETRS.load('Output/default/pcb153 run/dyn_out.cpk')
+	results = BETRS.load(filename_path)
 
-	air = results[2]['Pa'][77,:]
+	# Retrieve fugacities from relevant environmental media
+	# Format:
+	# results[compartment][units][grid_cell,:]
+	# Compartment = the environmental media (lower air, upper air, seawater, etc)
+	# Units - 'Pa' is fugacity - required for ACC-Human.
+	# grid_cell: The grid cell from the BETR-Research globe. Remember to subtract 1 from the actual zone due to indexing starting at 0.
+
+	air = results[2]['Pa'][77,:] # Lower air compartment, Fugacity (Pa), Region / grid cell from 78
 	seawater = results[5]['Pa'][75,:]
 	soil = results[6]['Pa'][77,:]
 	freshwater = results[4]['Pa'][77,:]
-	groundwater = freshwater
-
+	groundwater = freshwater # Assuming same contamination in groundwater as freshwater
 
 	# 	Open File for Output
 	#	Because ACC-Human is windows based, I must use the \r\n for End Of Line (EOL)
-	fugacityFile = open(chemicalName + '.txt', 'w+')
+	fugacity_file = open(chemicalName + '.txt', 'w+')
 
 	#	Write first 3 lines
-	fugacityFile.write('0730\r\n')
-	fugacityFile.write('1930\r\n')
-	fugacityFile.write('2100')
+	fugacity_file.write('0730\r\n')
+	fugacity_file.write('1930\r\n')
+	fugacity_file.write('2100')
 
 	# Loop through Fugacities and write to file
 	for f1, f2, f3, f4, f5 in zip(air, seawater, soil, freshwater, groundwater):
-		fugacityFile.write('\r\n')
-		fugacityFile.write(' ')
-		fugacityFile.write('{:.3E}'.format(float(f1)))
+		fugacity_file.write('\r\n')
+		fugacity_file.write(' ')
+		fugacity_file.write('{:.3E}'.format(float(f1)))
 
-		fugacityFile.write(' ')
-		fugacityFile.write('{:.3E}'.format(float(f2)))
+		fugacity_file.write(' ')
+		fugacity_file.write('{:.3E}'.format(float(f2)))
 
-		fugacityFile.write(' ')
-		fugacityFile.write('{:.3E}'.format(float(f3)))
+		fugacity_file.write(' ')
+		fugacity_file.write('{:.3E}'.format(float(f3)))
 
-		fugacityFile.write(' ')
-		fugacityFile.write('{:.3E}'.format(float(f4)))
+		fugacity_file.write(' ')
+		fugacity_file.write('{:.3E}'.format(float(f4)))
 
-		fugacityFile.write(' ')
-		fugacityFile.write('{:.3E}'.format(float(f5)))
+		fugacity_file.write(' ')
+		fugacity_file.write('{:.3E}'.format(float(f5)))
 
-	fugacityFile.close()
+	fugacity_file.close()
 
 ######################################################
 
-writeFugacityFile()
+write_fugacity_file()
 
 ######################################################
